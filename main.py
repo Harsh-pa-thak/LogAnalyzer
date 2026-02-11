e59ffdf2e845e21970ec51f6917e78cd658c3af1
Harsh Pathak <harphy45@gmail.com>
2026-02-11 18:02:32 +0530

    python backend sever inti

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,UploadFile,File
 
 app = FastAPI(title="Log Analyzer Agent")
 @app.get("/")
 def read_root():
     return {"message": "Log Analyzer Agent is running"}
+
